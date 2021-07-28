a97fbaf72135062d85ed35a56be8fa3e2a3793c7
liyusang1 <liyusang1@naver.com>
2021-07-28 17:29:40 +0900

    Add : Tree

---
+++ py/Tree.py
@@ -0,0 +1,124 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self, head):
+        self.head = head
+
+    def insert(self, value):
+        self.node = self.head
+        while True:
+            if value < self.node.value:
+                if self.node.left != None:
+                    self.node = self.node.left
+                else:
+                    self.node.left = Node(value)
+                    break
+            else:
+                if self.node.right != None:
+                    self.node = self.node.right
+                else:
+                    self.node.right = Node(value)
+                    break
+
+    def search(self, value):
+        self.node = self.head
+        while self.node:
+            if self.node.value == value:
+                return True
+            elif value < self.node.value:
+                self.node = self.node.left
+            else:
+                self.node = self.node.right
+        return False
+
+    def delete(self, value):
+        # 삭제할 노드 탐색
+        searched = False
+        self.current_node = self.head
+        self.parent = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                searched = True
+                break
+            elif value < self.current_node.value:
+                self.parent = self.current_node
+                self.current_node = self.current_node.left
+            else:
+                self.parent = self.current_node
+                self.current_node = self.current_node.right
+
+        if searched == False:
+            return False
+
+            # case1
+        if self.current_node.left == None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = None
+            else:
+                self.parent.right = None
+
+        # case2
+        elif self.current_node.left != None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.left
+            else:
+                self.parent.right = self.current_node.left
+        elif self.current_node.left == None and self.current_node.right != None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.right
+            else:
+                self.parent.right = self.current_node.right
+
+                # case 3
+        elif self.current_node.left != None and self.current_node.right != None:
+            # case3-1
+            if value < self.parent.value:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent.left = self.change_node
+                self.change_node.right = self.current_node.right
+                self.change_node.left = self.change_node.left
+            # case 3-2
+            else:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent.right = self.change_node
+                self.change_node.right = self.current_node.right
+                self.change_node.left = self.current_node.left
+
+        return True
+
+
+head = Node(1)
+BST = Tree(head)
+BST.insert(2)
+BST.insert(3)
+BST.insert(0)
+BST.insert(4)
+BST.insert(8)
+
+print(BST.search(4))
+print(BST.search(2))
+print(BST.search(3))
+print(BST.search(0))
+print(BST.search(8))
+print(BST.search(82132))
