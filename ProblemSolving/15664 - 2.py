54f599ac790b84c7d08e58b312bcafcfb005977e
liyusang1 <liyusang1@naver.com>
2021-08-18 21:55:43 +0900

    Add : BOJ 15664-2

---
+++ ProblemSolving/15664 - 2.py
@@ -0,0 +1,13 @@
+# 15664 itertool 풀이
+import itertools
+
+n, m = map(int, input().split())
+
+numberList = list(map(int, input().split()))
+numberList.sort()
+answer = list(itertools.combinations(numberList, m))
+
+answer = sorted(list(set(answer)))  # set 자료구조를 이용한 중복 제거
+
+for i in answer:
+    print(" ".join(map(str, i)))
