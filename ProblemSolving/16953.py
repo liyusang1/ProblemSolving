ffc9695fdf102a3d977f37d88dc58d20d801338d
liyusang1 <liyusang1@naver.com>
2021-10-04 21:52:24 +0900

    Add : BOJ 16953

--- ProblemSolving/16953.py
+++ ProblemSolving/16953.py
@@ -3,7 +3,7 @@ from collections import deque
 
 n, m = map(int, stdin.readline().split())
 
-
+# *** 무조건 값이 증가하기 때문에 사이클이 생기지 않아 visited가 필요없는 것이 Point!! ***
 def BFS():
     queue = deque()
     queue.append([n, 1])
