3add455ea5a2ed53794816469a115ba643e3747c
liyusang1 <liyusang1@naver.com>
2021-10-07 20:41:48 +0900

    Add : BOJ 2212

--- ProblemSolving/2212.py
+++ ProblemSolving/2212.py
@@ -3,19 +3,18 @@ from sys import stdin
 
 n = int(stdin.readline())
 m = int(stdin.readline())
+
+#예외 처리 중요 포인트
+if m>=n:
+    print(0)
+    exit()
 locationList = list(map(int, stdin.readline().split()))
 locationList.sort()
 
-distanceList = []
-
-# * 핵심아이디어 -> 오름차순으로 정렬을 한 후 각 위치의 길이를 저장해서 길이가 긴 거리를 m값에 따라 빼준다.
-for i in range(1, n):
-    distanceList.append(locationList[i] - locationList[i - 1])
+#핵심 아이디어 각 거리리의 차이를 계산한후 정렬하여 m값에 따라 큰것을 빼준다
+distanceList = [locationList[i] - locationList[i - 1] for i in range(1,n)]
 distanceList.sort(reverse=True)
 
 for _ in range(m - 1):
-    # **예외 처리 실수하지 말기.
-    if len(distanceList) == 0:
-        break
     distanceList.pop(0)
 print(sum(distanceList))
