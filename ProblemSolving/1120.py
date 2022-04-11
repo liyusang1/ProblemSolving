f08d8cb3c6e1444021a5166167f90e4ba5ba9867
liyusang1 <liyusang1@naver.com>
2022-04-11 21:06:07 +0900

    Add : BOJ 1120

---
+++ ProblemSolving/1120.py
@@ -0,0 +1,15 @@
+# 1120
+
+from sys import stdin
+
+a, b = stdin.readline().split()
+
+answer = []
+for i in range(len(b) - len(a) + 1):
+    count = 0
+    for j in range(len(a)):
+        if a[j] != b[i + j]:
+            count += 1
+    answer.append(count)
+
+print(min(answer))
